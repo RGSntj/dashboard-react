35447992ef27949a84a39d670736e0ca83540544
Maria <mfqsantosz@gmail.com>
2024-07-20 18:39:34 -0300

    creating Call Cards

--- src/app.tsx
+++ src/app.tsx
@@ -5,20 +5,129 @@ import { Call } from "./types/CallInterface";
 import { CallCard } from "./components/call-card";
 
 export function App() {
-  const {
-    data: calls,
-    isLoading,
-    error,
-  } = useQuery<Call[]>({ queryKey: ["calls"], queryFn: getCalls });
+
+  const calls: Call[] = [
+    {
+      id: "u87zgvsd0876dsyaag87",
+      category: "Hardware",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur
+        vel cupiditate odio quas rem! Illum ipsam asperiores obcaecati, reiciendis
+        eaque deserunt, recusandae autem, rem vel voluptatum in nisi hic!`,
+      authorId: `87asfd6y7av0867dasf098`,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      author: {
+        username: "Maria",
+      },
+    },
+    {
+      id: "u87zgvsd0876dsyaag87",
+      category: "Software",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur
+        vel cupiditate odio quas rem! Illum ipsam asperiores obcaecati, reiciendis
+        eaque deserunt, recusandae autem, rem vel voluptatum in nisi hic!`,
+      authorId: "87asfd6y7av0867dasf098",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      author: {
+        username: "Rodrygo",
+      },
+    },
+    {
+      id: "u87zgvsd0876dsyaag87",
+      category: "Hardware",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur
+        vel cupiditate odio quas rem! Illum ipsam asperiores obcaecati, reiciendis
+        eaque deserunt, recusandae autem, rem vel voluptatum in nisi hic!`,
+      authorId: `87asfd6y7av0867dasf098`,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      author: {
+        username: "Maria",
+      },
+    },
+    {
+      id: "u87zgvsd0876dsyaag87",
+      category: "Software",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur
+        vel cupiditate odio quas rem! Illum ipsam asperiores obcaecati, reiciendis
+        eaque deserunt, recusandae autem, rem vel voluptatum in nisi hic!`,
+      authorId: "87asfd6y7av0867dasf098",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      author: {
+        username: "Rodrygo",
+      },
+    },
+    {
+      id: "u87zgvsd0876dsyaag87",
+      category: "Hardware",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur
+        vel cupiditate odio quas rem! Illum ipsam asperiores obcaecati, reiciendis
+        eaque deserunt, recusandae autem, rem vel voluptatum in nisi hic!`,
+      authorId: `87asfd6y7av0867dasf098`,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      author: {
+        username: "Maria",
+      },
+    },
+    {
+      id: "u87zgvsd0876dsyaag87",
+      category: "Software",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur
+        vel cupiditate odio quas rem! Illum ipsam asperiores obcaecati, reiciendis
+        eaque deserunt, recusandae autem, rem vel voluptatum in nisi hic!`,
+      authorId: "87asfd6y7av0867dasf098",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      author: {
+        username: "Rodrygo",
+      },
+    },
+    {
+      id: "u87zgvsd0876dsyaag87",
+      category: "Hardware",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur
+        vel cupiditate odio quas rem! Illum ipsam asperiores obcaecati, reiciendis
+        eaque deserunt, recusandae autem, rem vel voluptatum in nisi hic!`,
+      authorId: `87asfd6y7av0867dasf098`,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      author: {
+        username: "Maria",
+      },
+    },
+    {
+      id: "u87zgvsd0876dsyaag87",
+      category: "Software",
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique tenetur
+        vel cupiditate odio quas rem! Illum ipsam asperiores obcaecati, reiciendis
+        eaque deserunt, recusandae autem, rem vel voluptatum in nisi hic!`,
+      authorId: "87asfd6y7av0867dasf098",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      author: {
+        username: "Rodrygo",
+      },
+    },
+  ];
+  // const {
+  //   data: calls,
+  //   isLoading,
+  //   error,
+  // } = useQuery<Call[]>({ queryKey: ["calls"], queryFn: getCalls });
 
   return (
     <div className="h-screen bg-zinc-900">
       <div className="space-y-4 p-5">
         <h1 className="font-bold text-zinc-200 text-lg uppercase">chamados</h1>
 
-        {calls?.map((call) => (
-          <CallCard call={call} />
-        ))}
+        <div className="flex flex-wrap gap-10 justify-center">
+          {calls?.map((call) => (
+            <CallCard call={call} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/call-card.tsx
+++ src/components/call-card.tsx
@@ -1,9 +1,17 @@
 import { Call } from "../types/CallInterface";
+import { CircleUserRound } from 'lucide-react'
 
 interface CallProps {
   call: Call;
 }
 
 export function CallCard({ call }: CallProps) {
-  return <p>Card</p>;
+  return(
+   <div className="bg-zinc-800 max-w-96 p-5 rounded-md shadow-3xl">
+    <h1 className="flex items-center gap-2 text-xl text-white"> <CircleUserRound/> {call.author.username}</h1>
+    <span className="text-zinc-400">{call.category}</span>
+    <p className="py-4 text-white">{call.description}</p>
+   </div>
+  )
 }
+
